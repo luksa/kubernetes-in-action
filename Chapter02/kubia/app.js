0d44037ce8ceacb02548496585eda275da5f8683
viral040392 <129819685+viral040392@users.noreply.github.com>
2023-05-30 13:21:26 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("Viral Shah" + "\n");
 };
 
 var www = http.createServer(handler);
